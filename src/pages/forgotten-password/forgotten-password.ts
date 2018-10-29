c980be4b6ac8def5766cf932679ae8c4c4265627
Prince Cuberdon <princecuberdon@bandcochon.fr>
2018-10-29 19:25:29 +0100

    Forgotten password

--- src/pages/forgotten-password/forgotten-password.ts
+++ src/pages/forgotten-password/forgotten-password.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams, AlertController } from 'ionic-angular';
+import { NavController, NavParams, AlertController, LoadingController, Loading } from 'ionic-angular';
 import { BandcochonProvider } from '../../providers/bandcochon/bandcochon';
 
 
@@ -18,19 +18,26 @@ export class ForgottenPasswordPage {
     public navCtrl: NavController,
     public navParams: NavParams,
     private bandcochon: BandcochonProvider,
-    private alertCtrl: AlertController) {
+    private alertCtrl: AlertController,
+    private loadingCtrl: LoadingController) {
   }
 
-  ionViewDidLoad() {
-  }
-  // FIXME : Ajouter une spinner pour l'attente
   onSendEmail() {
+    if (this.email.trim() === '') {
+      this.displayError("Vous n'avez entré aucune adresse email, ce qui reste problématique pour vous identifier.");
+      return;
+    }
+
+    const loading = this.displayLoading();
+
     this.bandcochon.forgotten(this.email)
       .then(() => {
         this.onIHaveACode()
       })
       .catch((err) => {
-        this.displayError(`Une erreur est survenue. Merci de réessayer ultérieurement.`);
+        this.displayError(`Cette adresse mail n'appartient pas à un utilisateur de bandcochon.`, 'Adresse inconnue');
+      }).then(() => {
+        loading.dismiss();
       });
   }
 
@@ -39,38 +46,56 @@ export class ForgottenPasswordPage {
   }
 
   onSendCode() {
+    if (this.password != this.confirm) {
+      this.displayError("Le mot de passe et sa confirmation sont différents");
+      return;
+    }
+
+    const loading = this.displayLoading();
+
     this.bandcochon.sendCode(this.code, this.email, this.password, this.confirm)
       .then(() => {
-        this.displaySuccess('Mot de passe remplacé avec succès');
+        this.displaySuccess('Mot de passe remplacé avec succès').then(() => {
+          this.navCtrl.popToRoot();
+        });
       })
       .catch((err) => {
-        this.displayError('Une erreur est survenue. Merci de réessayer ultérieurement.');
+        this.displayError('Mauvais code ou mauvaise adresse email.');
       })
       .then(() => {
-        this.navCtrl.first();
+        loading.dismiss();
       });
   }
 
   /* 
    * Display a more explicit error
    */
-  private displayError(msg: string): void {
+  private displayError(msg: string, title?: string): void {
     this.alertCtrl
       .create({
-        title: "Erreur serveur",
+        title: !!title ? title : "Erreur",
         subTitle: msg,
         "buttons": ["OK"],
       })
       .present();
   }
 
-  private displaySuccess(msg:string): void {
-    this.alertCtrl
+  private displaySuccess(msg: string): Promise<any> {
+    return this.alertCtrl
       .create({
         title: "Succès",
         subTitle: msg,
         "buttons": ["OK"],
       })
       .present();
+  }
+
+  private displayLoading(): Loading {
+    const loading = this.loadingCtrl.create({
+      content: "Un instant s'il vous plaît..."
+    });
+    loading.present();
+
+    return loading;
   }
 }
