e43d0675b537dcca1e15515953f76daf868a6af5
Prince Cuberdon <princecuberdon@bandcochon.fr>
2018-12-27 09:49:46 +0100

    Cleaning code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Platform, Events, NavController } from 'ionic-angular';
+import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -8,21 +8,15 @@ import { PingPage } from '../ping/ping';
 @Component({
   templateUrl: 'app.html'
 })
-export class MyApp  implements OnInit {
+export class MyApp  {
   rootPage: any = PingPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private events: Events) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       statusBar.styleDefault();
       splashScreen.hide();
     });
   }
 
-  ngOnInit() {
-    this.listenToEvents();
-  }
-
-  listenToEvents(): void {
-  }
 }
  
